4bd9f49c8fbe5796decc34d728a378649fc8de2d
HenskiS <59155518+HenskiS@users.noreply.github.com>
2023-11-07 11:05:41 -0800

    Users routes are protected, admin dash gets clients
    Server users router now checks for Bearer token in headers.
    Admin dashboard now fetches all users.

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -1,10 +1,36 @@
+import { Fragment, useEffect, useState } from "react";
+import axios from "../api/axios.js";
 
 const AdminDashboard = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const token = JSON.parse(sessionStorage.getItem('token'));
+                const config = {
+                    headers: { Authorization: `Bearer ${token}` }
+                };
+                const response = await axios.get("/users/", config);
+                console.log("got users info");
+                console.log(response);
+                setUsers(response.data);
+                //setEditClient(response.data);
+            } catch (err) { console.error(err); }
+        }
+        getUsers();
+    } ,[])
 
 
     return (
         <div className="admin content">
             <h1>Admin Dashboard</h1>
+            {users.map((user, index) => (
+                <Fragment key={index}>
+                    <p>{user.name}</p>
+                </Fragment>
+            ))}
         </div>
     );
 }
--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://192.168.1.184:3000',
     'http://192.168.1.136:3000',
+    'http://192.168.1.102:3000',
     'https://www.google.com'
 ]
 
