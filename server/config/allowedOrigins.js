3b87a541c76380b23f8bddb4e1673fcf5fe0330e
HenskiS <59155518+HenskiS@users.noreply.github.com>
2024-02-15 11:56:06 -0800

    Fixed index.html and icons issues

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,11 +1,13 @@
 const allowedOrigins = [
     'http://localhost',
+    'http://localhost:3001/',
     'https://ecsales.work',
     'https://www.ecsales.work',
     'http://192.168.*',
     'http://localhost:3000',
     'http://192.168.1.184:3000',
     'http://192.168.1.136:3000',
+    'http://192.168.1.138:3000',
     'http://192.168.1.102',
     'http://192.168.1.103:3000',
     'http://192.168.1.18:3000',
